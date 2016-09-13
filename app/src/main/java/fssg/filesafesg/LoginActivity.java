610ac009916f3e0a796def71564f7e2865940678
MrBread2000 <welsontn@gmail.com>
2016-09-13 15:27:48 +0800

    BREADO

--- app/src/main/java/fssg/filesafesg/LoginActivity.java
+++ app/src/main/java/fssg/filesafesg/LoginActivity.java
@@ -47,8 +47,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
-     * TODO: NOTHING AT ALL
-     * kokokokokokokoko 222222
+     * TODO: MAKE SUPE RBREAD
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
