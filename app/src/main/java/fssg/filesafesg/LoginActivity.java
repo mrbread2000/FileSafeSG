020985de2332762365cd6077e947485a529b9f1e
Kevin Sng <sngqizhi@gmail.com>
2016-09-13 15:18:00 +0800

    Test Branch Kevin

--- app/src/main/java/fssg/filesafesg/LoginActivity.java
+++ app/src/main/java/fssg/filesafesg/LoginActivity.java
@@ -48,6 +48,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
      * TODO: NOTHING AT ALL
+     * kokokokokokokoko
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
@@ -56,6 +57,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
+    private String kokokokoko = "koko";
 
     // UI references.
     private AutoCompleteTextView mEmailView;
