2e1de10968a32ba10c510022798427aec3897914
Kevin Sng <sngqizhi@gmail.com>
2016-09-13 15:53:06 +0800

    kokoko to lalala

--- app/src/main/java/fssg/filesafesg/LoginActivity.java
+++ app/src/main/java/fssg/filesafesg/LoginActivity.java
@@ -48,8 +48,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
      * TODO: NOTHING AT ALL
-     * kokokokokokokoko 222222
-     * kokokokokikikiki
+     * lalala 3
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
