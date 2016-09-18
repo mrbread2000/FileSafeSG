b47d6c7867e798c2272622b99d9b3cc65c47cb76
kevinsng <sngqizhi@gmail.com>
2016-09-19 05:12:43 +0800

    Register On database done

--- app/src/main/java/fssg/filesafesg/RegisterActivity.java
+++ app/src/main/java/fssg/filesafesg/RegisterActivity.java
@@ -29,7 +29,45 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText etName = (EditText) findViewById(R.id.etName);
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
-
         final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = etName.getText().toString();
+                final String username = etUsername.getText().toString();
+                final int age = Integer.parseInt(etAge.getText().toString());
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //Convert into JSON object
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                //Redirect to login page if successful
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
+                            } else {
+                                //If not successful, display error
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                //Create request
+                RegisterRequest registerRequest = new RegisterRequest(name, username, age, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 }
