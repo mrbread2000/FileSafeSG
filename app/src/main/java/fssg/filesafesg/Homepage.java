6ca6299ab8396ec5e6c8eec089b811dff2c686a8
Chris Phan <dapowerpig@hotmail.com>
2016-09-19 15:51:05 +0800

    gui for homepage

---
+++ app/src/main/java/fssg/filesafesg/Homepage.java
@@ -0,0 +1,26 @@
+package fssg.filesafesg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+      // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+
+    }
+   /*
+    public void goToPhoto (View view){
+        Intent intent = new Intent(this, goToPhoto.class);
+        startActivity(intent);
+    }
+    */
+}
