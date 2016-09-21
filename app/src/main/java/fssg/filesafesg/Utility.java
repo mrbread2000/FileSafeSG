be693c081772feb5357fbe0f628f9e0a449ad69a
MrBread2000 <welsontn@gmail.com>
2016-09-21 14:33:50 +0800

    updated crytopgrahy (not 100% done)

--- app/src/main/java/fssg/filesafesg/Utility.java
+++ app/src/main/java/fssg/filesafesg/Utility.java
@@ -10,7 +10,8 @@ public class Utility {
 
     private static MessageDigest md;
 
-    public static String hash(String input) {
+    //create message digest
+    private static void initialization(){
 
         if (md == null){
             try {
@@ -19,6 +20,33 @@ public class Utility {
                 Log.e("FileSafeSG", "No SHA-1 algorithm is found.");
             }
         }
+
+
+    }
+
+    //Convert hex to string
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    //hash using SHA-1
+    public static String hash(String sinput) {
+
+        initialization();
+
+        String hashed;
+        md.reset();
+        md.update(sinput.getBytes());
+        hashed = bytesToHex(md.digest());
+
+        return hashed;
 
     }
 
