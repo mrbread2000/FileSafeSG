5857346533f46e3f63209612ceda861c71b69138
annapoor001 <117106purplemate@gmail.com>
2016-10-12 17:38:24 +0800

    onpause is working

--- app/src/main/java/fssg/filesafesg/MainActivity.java
+++ app/src/main/java/fssg/filesafesg/MainActivity.java
@@ -5,18 +5,25 @@ package fssg.filesafesg;
  */
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
     Button audio, photos, videos, docs, encryptfiles;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        super.onPause();
+
+
         setContentView(R.layout.activity_main);
 
         audio = (Button) findViewById(R.id.audio);
