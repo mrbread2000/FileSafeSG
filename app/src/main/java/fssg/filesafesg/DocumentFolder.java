da6161499cb7355057ebf71c7022cfb391df13a0
Chris Phan <dapowerpig@hotmail.com>
2016-11-09 18:16:49 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/fssg/filesafesg/MainActivity.java
    #	app/src/main/java/fssg/filesafesg/VideoFolder.java
    #	app/src/main/java/fssg/filesafesg/docFolder.java
    #	app/src/main/java/fssg/filesafesg/encryptionFolder.java
    #	app/src/main/res/layout/activity_homepage.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/mipmap-hdpi/ic_launcher.png
    #	app/src/main/res/mipmap-mdpi/ic_launcher.png
    #	app/src/main/res/mipmap-xhdpi/ic_launcher.png
    #	app/src/main/res/mipmap-xxhdpi/ic_launcher.png
    #	app/src/main/res/mipmap-xxxhdpi/ic_launcher.png

--- app/src/main/java/fssg/filesafesg/DocumentFolder.java
+++ app/src/main/java/fssg/filesafesg/DocumentFolder.java
@@ -8,7 +8,6 @@ package fssg.filesafesg;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
